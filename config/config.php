e7beaf85d50f6b2ca97ad21d3585b5d8f0a9706f
AndreCandidoDev <andd68@hotmail.com>
2025-02-20 11:40:17 -0300

    Api example

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'sakila');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '123mudar');
+    
+    define('DEBUG', true); // Altere para 'false' no ambiente de produção
+?>
